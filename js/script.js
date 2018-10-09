5462c59f6083850eb31d92dfa3068c4ee896f0e3
Christopher Lauriano <christopherlauriano@gmail.com>
2018-10-09 12:18:58 -0700

    Third iteration

--- js/script.js
+++ js/script.js
@@ -1,33 +1,39 @@
 
-
+//Array array quotes is an array of objects with a quote and the source
 var quotes = [
   {
-    quote:"Be loyal to those who are loyal to you. ", name:"John Cena"
+    quote:"Be loyal to those who are loyal to you. ", source:"John Cena"
     },
   {
-    quote:"To all my little Hulkamaniacs, say your prayers, take your vitamins and you will never go wrong.", name:"Hulk Hogan"
+    quote:"To all my little Hulkamaniacs, say your prayers, take your vitamins and you will never go wrong.", source:"Hulk Hogan"
     },
   {
-      quote:"Oh it\'s true, it\'s damn true. ", name:"Kurt Angle"
+    quote:"Oh it\'s true, it\'s damn true. ", source:"Kurt Angle"
     },
   {
-      quote:"Everything is impossible, until someone crazy enough comes along and makes it possible. ", name:"CT Fletcher"
-    }
+    quote:"Everything is impossible, until someone crazy enough comes along and makes it possible.", source:"CT Fletcher"
+    },
+  {
+    quote:"You can do anything but not everything", source:"David Allen"
+  }
   ];
   console.log(quotes[1].quote);
 
+//function used to get a random number and return an object from the array
   function getRandomQuote (){
-   var randomNumber = Math.floor(Math.random() * quotes.length );
-    return "<p class = quote >" + quotes[randomNumber].quote + "</p>" + "<p class = source >" + quotes[randomNumber].name + "</p>";
-  
+    var randomNumber = Math.floor(Math.random() * quotes.length );
+    console.log(quotes[randomNumber]);
+    return quotes[randomNumber];
   }
-
-
+  
+  
+//function used to create the output being written on html page
   function printQuote (){
-    saying = getRandomQuote();
+    var saying = getRandomQuote();
     var output = document.getElementById('quote-box');
-    output.innerHTML = saying
+    output.innerHTML = "<p class = quote >" + saying.quote + "</p>" + "<p class = source >" + saying.source + "</p>";
   }
 
   
   document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+  
